4ab5c1b095eb0c032a1f1addf8b71464bb604946
Arch <tsieyy@qq.com>
2023-10-09 18:35:15 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+import os
+
+
+# Press Ctrl+F5 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # print_hi('PyCharm')
+
+    os.system('streamlit run chat_bot/cb_demo.py')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
