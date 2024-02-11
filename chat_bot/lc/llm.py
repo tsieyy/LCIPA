55642684bda0c735bfc589d45c8fcaebbd1c80bf
Arch <tsieyy@qq.com>
2024-02-11 17:31:24 +0800

    refactor this project, make it more readable

--- chat_bot/lc/llm.py
+++ chat_bot/lc/llm.py
@@ -1,4 +1,5 @@
 from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
 
-
+chat = ChatOpenAI(temperature=0)
 llm = OpenAI(temperature=0, streaming=True)
--- pages/2_Chat_bot.py
+++ pages/2_Chat_bot.py
@@ -1,50 +1,10 @@
 import streamlit as st
-from langchain.agents import initialize_agent, AgentType
-from langchain.agents import AgentExecutor, create_openai_tools_agent
 from langchain.callbacks.streamlit import StreamlitCallbackHandler
-from langchain.memory import ConversationBufferMemory
-from langchain_community.chat_message_histories import StreamlitChatMessageHistory
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables import RunnableConfig
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage
 
 from utils.login import make_sure_login
-
-from chat_bot.cb_demo import llm, tools, chat
-
-
-# Set up memory
-msgs = StreamlitChatMessageHistory(key="langchain_messages")
-if len(msgs.messages) == 0:
-    msgs.add_ai_message("How can I help you?")
+from chat_bot.cb_demo import llm, tools, chat, msgs, chain_with_history
 
 view_messages = st.expander("View the message contents in session state")
-
-# Adapted from https://smith.langchain.com/hub/hwchase17/openai-tools-agent
-prompt_ = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You are a helpful assistant. You may not need to use tools for every query - the user may just want to chat! But when they ask you about something that happened in real life, you may need to use tools to ensure that your answers are accurate.",
-        ),
-        MessagesPlaceholder(variable_name="history"),
-        ("human", "{input}"),
-        MessagesPlaceholder(variable_name="agent_scratchpad"),
-    ]
-)
-
-agent = create_openai_tools_agent(chat, tools, prompt_)
-agent_executor = AgentExecutor(agent=agent, tools=tools, )
-
-chain_with_history = RunnableWithMessageHistory(
-    agent_executor,
-    lambda session_id: msgs,
-    input_messages_key="input",
-    output_messages_key="output",
-    history_messages_key="history",
-)
 
 if make_sure_login():
     # Render current messages from StreamlitChatMessageHistory
