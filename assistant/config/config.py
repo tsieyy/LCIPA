4230823d72bf796a7ddcb59c505c0ed4eec0d5cb
Arch <tsieyy@qq.com>
2024-05-29 15:44:17 +0800

    add new config model for assistant package

---
+++ assistant/config/config.py
@@ -0,0 +1,18 @@
+import configparser
+
+
+class Config:
+    """Config class for Assistant."""
+
+    def __init__(self):
+        self.config_file_path = "config/config.ini"
+        self.load_config_file()
+
+    def load_config_file(self):
+        """Load the config file."""
+        if self.config_file_path is None:
+            return None
+        config = configparser.ConfigParser()
+        config.read(self.config_file_path)
+        for key, value in config.items("assistant"):
+            setattr(self, key.lower(), value)
