dffdf0fb42ad04500e2e647e6afe18783d39d22c
Arch <tsieyy@qq.com>
2024-05-30 17:28:12 +0800

    renew dev chat_bot module

---
+++ chat_bot/graph/utils.py
@@ -0,0 +1,176 @@
+
+from langchain_core.messages import HumanMessage
+
+from langchain.agents import AgentExecutor, create_openai_functions_agent, create_openai_tools_agent
+from langchain.output_parsers.openai_functions import JsonOutputFunctionsParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+from typing import List
+
+from chat_bot.graph.state import State
+from chat_bot.graph.tool import WORKING_DIRECTORY
+
+
+def create_agent(llm: ChatOpenAI, tools: list, system_prompt: str):
+    # Each worker node will be given a name and some tools.
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                system_prompt,
+            ),
+            MessagesPlaceholder(variable_name="messages"),
+            MessagesPlaceholder(variable_name="agent_scratchpad"),
+        ]
+    )
+    agent = create_openai_tools_agent(llm, tools, prompt)
+    executor = AgentExecutor(agent=agent, tools=tools)
+    return executor
+
+
+def create_team_agent(
+        llm: ChatOpenAI,
+        tools: list,
+        system_prompt: str,
+):
+    """Create a function-calling agent and add it to the graph."""
+    system_prompt += "\nWork autonomously according to your specialty, using the tools available to you."
+    " Do not ask for clarification."
+    " Your other team members (and other teams) will collaborate with you with their own specialties."
+    " You are chosen for a reason! You are one of the following team members: {team_members}."
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                system_prompt,
+            ),
+            MessagesPlaceholder(variable_name="messages"),
+            MessagesPlaceholder(variable_name="agent_scratchpad"),
+        ]
+    )
+    agent = create_openai_functions_agent(llm, tools, prompt)
+    executor = AgentExecutor(agent=agent, tools=tools)
+    return executor
+
+
+def agent_node(state, agent, name):
+    result = agent.invoke(state)
+    return {"messages": [HumanMessage(content=result["output"], name=name)]}
+
+
+def create_team_supervisor(llm: ChatOpenAI, system_prompt, members):
+    """An LLM-based router."""
+    options = ["FINISH"] + members
+    function_def = {
+        "name": "route",
+        "description": "Select the next role.",
+        "parameters": {
+            "title": "routeSchema",
+            "type": "object",
+            "properties": {
+                "next": {
+                    "title": "Next",
+                    "anyOf": [
+                        {"enum": options},
+                    ],
+                },
+            },
+            "required": ["next"],
+        },
+    }
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder(variable_name="messages"),
+            (
+                "system",
+                "Given the conversation above, who should act next?"
+                " Or should we FINISH? Select one of: {options}",
+            ),
+        ]
+    ).partial(options=str(options), team_members=", ".join(members))
+    return (
+            prompt
+            | llm.bind_functions(functions=[function_def], function_call="route")
+            | JsonOutputFunctionsParser()
+    )
+
+
+def create_supervisor(llm: ChatOpenAI, system_prompt, members, teams=None):
+    if teams is None:
+        teams = []
+    options = ["FINISH"] + members + teams
+    function_def = {
+        "name": "route",
+        "description": "Select the next role.",
+        "parameters": {
+            "title": "routeSchema",
+            "type": "object",
+            "properties": {
+                "next": {
+                    "title": "Next",
+                    "anyOf": [
+                        {"enum": options},
+                    ],
+                },
+            },
+            "required": ["next"],
+        },
+    }
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder(variable_name="messages"),
+            (
+                "system",
+                "Given the conversation above, who should act next?"
+                " Or should we FINISH? Select one of: {options}",
+            ),
+        ]
+    ).partial(options=str(options), members=", ".join(members+teams))
+    return (
+            prompt
+            | llm.bind_functions(functions=[function_def], function_call="route")
+            | JsonOutputFunctionsParser()
+    )
+
+
+# The following functions interoperate between the top level graph state
+# and the state of the research sub-graph
+# this makes it so that the states of each graph don't get intermixed
+def enter_chain(message: str, members: List[str]):
+    results = {
+        "messages": [HumanMessage(content=message)],
+        "team_members": ", ".join(members),
+    }
+    return results
+
+
+# This will be run before each worker agent begins work
+# It makes it so they are more aware of the current state
+# of the working directory.
+def prelude(state):
+    written_files = []
+    if not WORKING_DIRECTORY.exists():
+        WORKING_DIRECTORY.mkdir()
+    try:
+        written_files = [
+            f.relative_to(WORKING_DIRECTORY) for f in WORKING_DIRECTORY.rglob("*")
+        ]
+    except:
+        pass
+    if not written_files:
+        return {**state, "current_files": "No files written."}
+    return {
+        **state,
+        "current_files": "\nBelow are files your team has written to the directory:\n"
+        + "\n".join([f" - {f}" for f in written_files]),
+    }
+
+
+def get_last_message(state: State) -> str:
+    return state["messages"][-1].content
+
+
+def join_graph(response: dict):
+    return {"messages": [response["messages"][-1]]}
